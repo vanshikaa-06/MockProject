b7d7265182f38fdda341181f3de3faf6af0acd6a
vanshikaa-06 <121275602+vanshikaa-06@users.noreply.github.com>
2023-02-15 19:28:11 +0530

    Base Repo
    Added Basic Repo of project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Education } from './Education';
+
+function App() {
+  return (
+    <Education></Education>
+  );
+}
+
+export default App;
